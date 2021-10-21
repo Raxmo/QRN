6b28463f32d74efbafe4907dcdca3ed56dd0ce2a
Raxmo <maxsroe@gmail.com>
2021-10-21 17:50:03 -0600

    Add project files.

---
+++ QRN/NOINCLUDE/TODO.md
@@ -0,0 +1,32 @@
+﻿# QRN:  Dead Air
+  This is a horror game rendered entirely in static
+
+# TODO:
+  - [ ] Game
+    - [ ] Controls
+      - [ ] Mouse
+        - [ ] Look left/right
+        - [ ] ? Look up/down
+      - [ ] Keyboard
+        - [ ] WASD movement
+        - [ ] E to interact
+    - [ ] Rendering
+      - [ ] Raycast
+        - [ ] VS tilemap
+        - [ ] Sprites
+      - [ ] Static
+      - [ ] SDF
+        - [ ] Text
+        - [ ] Sprites
+    - [ ] Data
+      - [ ] Map Image
+      - [ ] Textures
+    - [ ] Interactions
+      - [ ] Doors
+      - [ ] Notes
+      - [ ] Entities
+    - [ ] Needed Classes
+      - [ ] Animation
+      - [ ] Player
+      - [ ] Entity
+      - [ ] Interactable
---
+++ QRN/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using OpenTK.Windowing.Desktop;
+using OpenTK.Mathematics;
+using OpenTK.Windowing.Common;
+
+namespace QRN
+{
+	class Program
+	{
+		#region Entry Point
+		static void Main(string[] args)
+		{
+			#region Boilerplate
+			// declarations
+			GameWindowSettings GWs = GameWindowSettings.Default;
+			NativeWindowSettings NWs = NativeWindowSettings.Default;
+
+			// Game Window settings
+			GWs.IsMultiThreaded = false;
+			GWs.RenderFrequency = 60;
+			GWs.UpdateFrequency = 60;
+
+			// Native Window Setings
+			NWs.APIVersion = Version.Parse("4.1.0");
+			NWs.Size = new Vector2i(1600, 900);
+			NWs.Title = "QRN:  Dead Air";
+
+			// create window
+			GameWindow GW = new GameWindow(GWs, NWs);
+			GW.UpdateFrame += (FrameEventArgs args) =>
+			{
+				// Put game loop here
+			};
+
+			// Run window
+			GW.Run();
+			#endregion
+		}
+		#endregion
+	}
+}
